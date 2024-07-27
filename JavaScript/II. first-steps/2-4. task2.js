0c4f139ab64e87f44581eebb2684225069cab24b
Thoughtful-Lover <isso8336@gmail.com>
2024-07-27 23:10:19 +0900

    240727_study_in_Hongdae

---
+++ JavaScript/II. first-steps/2-4. task2.js
@@ -0,0 +1,5 @@
+// 1. 우리가 살고있는 행성(planet)의 이름을 값으로 가진 변수
+const ourPlanet = "Earth"; // 지구라는 이름은 변하지 않으므로 const로 선언
+
+// 2. 현재 접속중인 사용자(user)의 이름(name)을 저장하는 변수
+let currentUserName; // 현재 접속중인 사용자 이름은 변할 수 있기 때문에 let으로 선언
---
+++ JavaScript/README.md
@@ -0,0 +1,3 @@
+# JavaScript
+* React Native 학습을 시작하며 js 복습을 위해 문서를 생성하였습니다.
+* <리액트 네이티브를 다루는 기술> 이라는 책에서 추천 받은 [사이트](https://ko.javascript.info/)를 참고하였습니다.
